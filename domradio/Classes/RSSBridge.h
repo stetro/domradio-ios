0500686cc159dff92edba572d69aa675020d5b2b
Steffen Tröster <steffen.troester@googlemail.com>
2015-05-06 11:52:03 +0200

    news loading spinner

--- domradio/Classes/RSSBridge.h
+++ domradio/Classes/RSSBridge.h
@@ -3,7 +3,6 @@
 //  domradio
 //
 //  Created by Steffen Tröster on 26/04/2015.
-//  Copyright (c) 2015 Facebook. All rights reserved.
 //
 
 #ifndef domradio_RSSBridge_h
--- domradio/app/components/DomradioNewsDetail.js
+++ domradio/app/components/DomradioNewsDetail.js
@@ -3,15 +3,31 @@
 var React = require('react-native');
 var ProgressBar = require('react-native-progress-bar');
 
-var { WebView, View, StyleSheet } = React;
+var { WebView, View, StyleSheet, AlertIOS, ActivityIndicatorIOS } = React;
 
 var DomradioNewsDetail = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <WebView url={this.props.data.link} style={styles.webView}/>
+        <WebView 
+          startInLoadingState={true}
+          onNavigationStateChange={this.onNavigationStateChange}
+          renderError={this.renderError}
+          url={this.props.data.link} 
+          style={styles.webView}/>
       </View>
       );
+  },
+  onNavigationStateChange: function(navState) {
+    console.log(navState);
+    if(navState.title == ''){
+      console.log('loading ...');
+    }else{
+      console.log(navState.title + " - loading: " + navState.loading);
+    }
+  },
+  renderError:function(error) {
+    AlertIOS.alert('Fehler', 'Das Laden des Artikels ist fehlgeschlagen.');
   }
 });
 
