ad886c4a1f3a94323a9f9e54b3f4ad43b4c18e96
Steffen Tröster <stetro@Steffens-MacBook-Air.local>
2015-06-25 13:31:02 +0200

    working detail view and first approach of player

---
+++ domradio-ios/Controller/NewsDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsDetailViewController.swift
+//  domradio-ios
+//
+//  Created by Steffen Tröster on 24/06/15.
+//  Copyright (c) 2015 Steffen Tröster. All rights reserved.
+//
+
+import Cocoa
+
+class NewsDetailViewController: NSObject {
+
+}
